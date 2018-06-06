289aca26c29deec769bddcace6543b960a5f07f1
jjoskey <alexander.s.ilyin@gmail.com>
2018-06-06 19:52:32 +0500

    1. Созданы файлы для главного окна: fe_main_window.ui (интерфейс Qt) и fe_main_window.py 2. Сделана связь между файлами

---
+++ fe_main_window.py
@@ -0,0 +1,16 @@
+"""
+
+Frontend First Interface for Chapaev Project
+
+"""
+
+
+import sys
+from PyQt5 import uic, QtWidgets
+
+
+chapaev = QtWidgets.QApplication(sys.argv)
+main_window = uic.loadUi('fe_main_window.ui')
+
+main_window.show()
+sys.exit(chapaev.exec_())
