a2cbc293c8c72e204d41a7abebd9d315749f9e44
Oper18 <genovikov93@gmail.com>
2018-06-07 15:41:28 +0300

    Solve problem with not stoped motion of dice. Next step will be in adding strengh of pushing dice.

--- models_scripts/motion/power.py
+++ models_scripts/motion/power.py
@@ -7,5 +7,5 @@ actuator = controller.actuators["Motion"] # the name of the Action actuator
 if controller.sensors["Keyboard"].positive: # trigger when buton pressed
 
     #Set all the properties you want
-    actuator.dLoc = [0.1, 0, 0]
+    actuator.dLoc = [0.01, 0, 0]
     controller.activate(actuator)
