03042e7d16883520daef35355d0ddf256191a96e
Oper18 <genovikov93@gmail.com>
2018-06-07 21:10:51 +0300

    Have some troubles in last time and didn't save any changes. Now save all changes with motion. Next step will set power of pushing dice.

--- models_scripts/motion/power.py
+++ models_scripts/motion/power.py
@@ -2,10 +2,21 @@ import bge
 
 controller = bge.logic.getCurrentController()
 
+motion = False
+
 actuator = controller.actuators["Motion"] # the name of the Action actuator
 
 if controller.sensors["Keyboard"].positive: # trigger when buton pressed
-
+    motion = True
+    
+if motion is True:
     #Set all the properties you want
     actuator.dLoc = [0.01, 0, 0]
     controller.activate(actuator)
+    controller.deactivate(actuator)
+    motion = False
+    
+else:
+    actuator.dLoc = [0, 0, 0]
+    controller.activate(actuator)
+    controller.deactivate(actuator)
