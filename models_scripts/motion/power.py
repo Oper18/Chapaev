dc2b465feea0733042f526607922b4124775b965
Oper18 <genovikov93@gmail.com>
2018-06-07 10:11:45 +0300

    Added one script. There are troubles with motion. Dice don't stop after starting. Don't how to solve it.

---
+++ models_scripts/motion/power.py
@@ -0,0 +1,11 @@
+import bge
+
+controller = bge.logic.getCurrentController()
+
+actuator = controller.actuators["Motion"] # the name of the Action actuator
+
+if controller.sensors["Keyboard"].positive: # trigger when buton pressed
+
+    #Set all the properties you want
+    actuator.dLoc = [0.1, 0, 0]
+    controller.activate(actuator)
