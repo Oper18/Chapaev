1191483716e391a486251b4bd50155baba4c7a39
Oper18 <genovikov93@gmail.com>
2018-06-14 13:59:50 +0300

    Arrow rotate around the dice, detect that alpha = 63 when arrow rotate at 180 degrees if step is 0.05. Move rotation func to power.py

--- models_scripts/motion/power.py
+++ models_scripts/motion/power.py
@@ -1,6 +1,9 @@
 import bge
 import time
 
+push = False
+alpha = 0
+
 def Motion():
     controller = bge.logic.getCurrentController()
     
@@ -22,5 +25,38 @@ def Motion():
     else:
         #print("False")
         actuator.dLoc = [0, 0, 0]
+        controller.activate(actuator)
+        controller.deactivate(actuator)
+
+def RotationArch():
+    global push, alpha
+    
+    alpha = alpha + 1
+    
+    controller = bge.logic.getCurrentController()
+    
+    if controller.sensors["Keyboard"].positive:
+        push = True
+    
+    actuator = controller.actuators["RotArch"]
+
+    if push is False:
+        controller.sensors["Always"].usePosPulseMode = True
+        z = 0.05
+    else:
+        controller.sensors["Always"].usePosPulseMode = False
+        z = 0
+    
+    # alpha = 62 is analog of 180 degrees rotation when step (z) is 0.05
+        
+    controller.sensors["Always"].tap = True
+    
+    if controller.sensors["Always"].status == 1:
+        actuator.dRot = [0, 0, z]
+        controller.activate(actuator)
+        controller.deactivate(actuator)
+        
+    elif controller.sensors["Always"].status == 3:
+        actuator.dRot = [0, 0, 0]
         controller.activate(actuator)
         controller.deactivate(actuator)
